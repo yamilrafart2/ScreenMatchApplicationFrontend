42d3c1194ecc26467a217561a1975cf51b35c354
yamilrafart2 <yamilrafart2>
2026-02-13 17:56:14 -0300

    feat: agrega scripts de series.js para la lógica de la página detalles.html

---
+++ scripts/series.js
@@ -0,0 +1,95 @@
+import getDatos from "./getDatos.js";
+
+const params = new URLSearchParams(window.location.search);
+const serieId = params.get('id');
+const listaTemporadas = document.getElementById('temporadas-select');
+const fichaSerie = document.getElementById('temporadas-episodios');
+const fichaDescripcion = document.getElementById('ficha-descripcion');
+
+// Función para cargar temporadas
+function cargarTemporadas() {
+    getDatos(`/series/${serieId}/temporadas/todas`)
+        .then(data => {
+            const temporadasUnicas = [...new Set(data.map(temporada => temporada.temporada))];
+            // Limpia las opciones existentes
+            listaTemporadas.innerHTML = '';
+
+            const optionDefault = document.createElement('option');
+            optionDefault.value = '';
+            optionDefault.textContent = 'Seleccione la temporada'
+            listaTemporadas.appendChild(optionDefault); 
+           
+            temporadasUnicas.forEach(temporada => {
+                const option = document.createElement('option');
+                option.value = temporada;
+                option.textContent = temporada;
+                listaTemporadas.appendChild(option);
+            });
+
+            const optionTodos = document.createElement('option');
+            optionTodos.value = 'todas';
+            optionTodos.textContent = 'Todas las temporadas'
+            listaTemporadas.appendChild(optionTodos); 
+        })
+        .catch(error => {
+            console.error('Error al obtener temporadas:', error);
+        });
+}
+
+// Función para cargar episodios de una temporada
+function cargarEpisodios() {
+    getDatos(`/series/${serieId}/temporadas/${listaTemporadas.value}`)
+        .then(data => {
+            const temporadasUnicas = [...new Set(data.map(temporada => temporada.temporada))];
+            fichaSerie.innerHTML = ''; 
+            temporadasUnicas.forEach(temporada => {
+                const ul = document.createElement('ul');
+                ul.className = 'episodios-lista'; 
+
+                const episodiosTemporadaAtual = data.filter(serie => serie.temporada === temporada);
+
+                const listaHTML = episodiosTemporadaAtual.map(serie => `
+                    <li>
+                        ${serie.numeroEpisodio} - ${serie.titulo}
+                    </li>
+                `).join('');
+                ul.innerHTML = listaHTML;
+                
+                const paragrafo = document.createElement('p');
+                paragrafo.textContent = `Temporada ${temporada}`;
+                fichaSerie.appendChild(paragrafo);
+                fichaSerie.appendChild(ul);
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener episodios:', error);
+        });
+}
+
+// Función para cargar informaciones de la serie
+function cargarInfoSerie() {
+    getDatos(`/series/${serieId}`)
+        .then(data => {
+            fichaDescripcion.innerHTML = `
+                <img src="${data.poster}" alt="${data.titulo}" />
+                <div class="detalle__descripcion__texto">
+                    <h2>${data.titulo}</h2>
+                    <div>
+                        <p><b>Evaluación:</b> ${data.evaluacion}</p>
+                        <p>${data.sinopsis}</p>
+                        <p><b>Actores:</b> ${data.actores}</p>
+                    </div>
+                </div>
+            `;
+        })
+        .catch(error => {
+            console.error('Error al obtener informaciones de la serie:', error);
+        });
+}
+
+// Adiciona listener de evento para el elemento select
+listaTemporadas.addEventListener('change', cargarEpisodios);
+
+// Carga las informaciones de la série y las temporadas cuando la página carga
+cargarInfoSerie();
+cargarTemporadas();
